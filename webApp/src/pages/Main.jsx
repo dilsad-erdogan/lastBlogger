1a9b3b3d633bb6d98004f69edb9e57a90f8e15c4
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-01-31 16:31:36 +0300

    logout

--- webApp/src/pages/Main.jsx
+++ webApp/src/pages/Main.jsx
@@ -1,8 +1,33 @@
+import { Toaster } from "react-hot-toast"
+import DarkMode from "../components/DarkMode"
+import { useNavigate } from "react-router-dom";
+import { logout } from "../firebase/auth/login";
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        await logout();
+        navigate('/login');
+    };
+
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <Toaster position='top-right' />
+
+            {/* Dark Mode and Logout */}
+            <div className='flex gap-6 items-center mt-5'>
+                <DarkMode />
+
+                <button onClick={handleLogout} className="text-white bg-blue-500 hover:bg-blue-800 font-medium rounded-lg w-full text-sm p-2 mt-4 px-5 me-2">Log out</button>
+            </div>
+
+            {/* Main Container */}
+            <div className='flex flex-row justify-center items-center text-center w-full max-w-[1200px] mt-10'>
+                Main
+            </div>
+        </div>
+    )
 }
 
 export default Main
