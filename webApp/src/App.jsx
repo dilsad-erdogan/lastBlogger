c9f228937d0078e04b03e9261bb00476554c5800
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-01-31 13:15:50 +0300

    page route

--- webApp/src/App.jsx
+++ webApp/src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Main from './pages/Main';
+import Profile from './pages/Profile';
+import Comment from './pages/Comment';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="h-screen">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Navigate to="/login" />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/main' element={<Main />} />
+          <Route path='/comment' element={<Comment />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
