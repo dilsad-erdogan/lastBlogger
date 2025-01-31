c30dd1caba891935d278cd1bca5fd93f1a2fb916
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-01-31 16:19:13 +0300

    log firebase connecting

--- webApp/src/components/LoginForm.jsx
+++ webApp/src/components/LoginForm.jsx
@@ -1,16 +1,37 @@
 import { useState } from "react";
 import { FaGoogle } from "react-icons/fa";
+import { login, loginWithGoogle } from "../firebase/auth/login";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
+    const navigate = useNavigate();
+
     const [email, setEmail] = useState(''); 
     const [password, setPassword] = useState(''); 
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const user = await login(email, password);
+        if(user) {
+            navigate('/main');
+        }
+    };
+
+    const handleGoogleSubmit = async () => {
+        const user = await loginWithGoogle();
+        console.log(user);
+        if(user) {
+            console.log('main')
+            navigate('/main');
+        }
+    };
 
     return (
         <div className="p-5">
             <h1 className="font-bold text-4xl text-blue-500 dark:text-blue-300" style={{ fontFamily: "'Lucida Handwriting', cursive" }}>Login</h1>
 
             {/* Form */}
-            <form className="flex flex-col gap-5 w-full mb-5 mt-5">
+            <form className="flex flex-col gap-5 w-full mb-5 mt-5" onClick={handleSubmit}>
                 <input type="email" className="bg-blue-100 dark:bg-blue-900 border border-black dark:border-white text-lg rounded-lg block w-full p-2.5" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
                 <input type="password" className="bg-blue-100 dark:bg-blue-900 border border-black dark:border-white text-lg rounded-lg block w-full p-2.5" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
                 <button disabled={!email || !password} type="submit" className="text-white bg-blue-500 hover:bg-blue-800 disabled:bg-blue-300 font-medium rounded-lg w-full text-sm px-5 py-2.5 me-2 mb-2">Log in</button>
@@ -24,7 +45,7 @@ const LoginForm = () => {
             </div>
 
             {/* Login Google */}
-            <div className="flex justify-center items-center p-4 gap-5">
+            <div className="flex justify-center items-center p-4 gap-5" onClick={handleGoogleSubmit}>
                 <FaGoogle className="text-2xl" />
                 <p className="text-blue-500 dark:text-blue-300">Log in with Google</p>
             </div>
--- webApp/src/components/RegisterForm.jsx
+++ webApp/src/components/RegisterForm.jsx
@@ -1,20 +1,30 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { register } from "../firebase/auth/register";
 
 const RegisterForm = () => {
+    const navigate = useNavigate();
+
     const [name, setName] = useState(''); 
     const [email, setEmail] = useState(''); 
-    const [password, setPassword] = useState(''); 
+    const [password, setPassword] = useState('');
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await register(name, email, password);
+        navigate('/login');
+    };
   
     return (
         <div className="p-5">
             <h1 className="font-bold text-4xl text-blue-500 dark:text-blue-300" style={{ fontFamily: "'Lucida Handwriting', cursive" }}>Register</h1>
 
             {/* Form */}
-            <form className="flex flex-col gap-5 w-full mb-5 mt-5">
+            <form className="flex flex-col gap-5 w-full mb-5 mt-5" onSubmit={handleSubmit}>
                 <input type="text" className="bg-blue-100 dark:bg-blue-900 border border-black dark:border-white text-lg rounded-lg block w-full p-2.5" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
                 <input type="email" className="bg-blue-100 dark:bg-blue-900 border border-black dark:border-white text-lg rounded-lg block w-full p-2.5" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
                 <input type="password" className="bg-blue-100 dark:bg-blue-900 border border-black dark:border-white text-lg rounded-lg block w-full p-2.5" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                <button disabled={!email || !password} type="submit" className="text-white bg-blue-500 hover:bg-blue-800 disabled:bg-blue-300 font-medium rounded-lg w-full text-sm px-5 py-2.5 me-2 mb-2">Log in</button>
+                <button disabled={!email || !password} type="submit" className="text-white bg-blue-500 hover:bg-blue-800 disabled:bg-blue-300 font-medium rounded-lg w-full text-sm px-5 py-2.5 me-2 mb-2">Register</button>
             </form>
         </div>
     )
