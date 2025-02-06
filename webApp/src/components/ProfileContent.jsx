c732493530971cee36ae15b494c2dc5cc1c32d81
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-02-06 21:27:45 +0300

    profile firebase

--- webApp/src/components/ProfileContent.jsx
+++ webApp/src/components/ProfileContent.jsx
@@ -1,41 +1,64 @@
-import { useState } from "react";
-import { FaRegCommentDots, FaRegHeart } from "react-icons/fa";
+import { useEffect, useState } from "react";
 import { SlCalender } from "react-icons/sl";
 import EditProfile from "./EditProfile";
+import { fetchUserPosts } from "../firebase/post/post";
+import { fetchUserById } from "../firebase/auth/user";
 
 const ProfileContent = () => {
     const [openModal, setOpenModal] = useState(false);
+    const [userPosts, setUserPosts] = useState([]);
+    const [user, setUser] = useState(null);
 
     const handleOpenModal = () => {
         setOpenModal(true);
     };
 
+    useEffect(() => {
+        const getUserPosts = async () => {
+          const posts = await fetchUserPosts(JSON.parse(localStorage.getItem('user')).uid);
+          setUserPosts(posts);
+        };
+    
+        const fetchUserData = async () => {
+          const storedUser = JSON.parse(localStorage.getItem("user"));
+          if (storedUser && storedUser.uid) {
+            const firebaseUser = await fetchUserById(storedUser.uid);
+            if (firebaseUser) {
+              setUser(firebaseUser);
+            }
+          }
+        };
+    
+        getUserPosts();
+        fetchUserData();
+      }, []);
+
     return (
         <div className="w-full">
             {/* Text */}
             <div className="w-full text-left border-b-2 border-black dark:border-white">
-                <p className="text-2xl font-bold mb-2">Alex</p>
+                <p className="text-2xl font-bold mb-2">{user?.name}</p>
             </div>
 
             {/* User Content */}
             <div className="w-full p-5 grid grid-cols-1 lg:grid-cols-2 text-left border-b-2 border-black dark:border-white">
                 {/* User Info */}
                 <div className="w-full">
-                    <h1 className="text-2xl font-bold">Alex</h1>
-                    <p className="text-sm text-gray-700 dark:text-gray-500">@alex</p>
-                    <p className="mt-4 font-bold">Hello, my name is Alex!</p>
+                    <h1 className="text-2xl font-bold">{user?.name}</h1>
+                    <p className="text-sm text-gray-700 dark:text-gray-500">{user?.email}</p>
+                    <p className="mt-4 font-bold">{user?.bio}</p>
                     <div className="flex items-center gap-4 mt-4 text-gray-700 dark:text-gray-500">
                         <SlCalender />
-                        <p className="text-sm">Joined March 2024</p>
+                        <p className="text-sm">Joined {new Date(user?.createdAt).toLocaleString()}</p>
                     </div>
                     <div className="flex gap-4 mt-4">
                         <div className="flex gap-1">
-                            <p className="font-bold">0</p>
+                            <p className="font-bold">{user?.followers?.length || 0}</p>
                             <p className="text-gray-700 dark:text-gray-500">Followers</p>
                         </div>
 
                         <div className="flex gap-1">
-                            <p className="font-bold">0</p>
+                            <p className="font-bold">{user?.following?.length || 0}</p>
                             <p className="text-gray-700 dark:text-gray-500">Following</p>
                         </div>
                     </div>
@@ -49,71 +72,13 @@ const ProfileContent = () => {
 
             {/* Tweets area */}
             <div className="flex-1 text-left mt-5">
-                {/* Tweet 1 */}
-                <div className="flex-1 text-left border-b-2 border-black dark:border-white mt-5">
-                    <div className="flex gap-2 ml-5 items-center">
-                        <h1 className="text-lg font-bold">Alex</h1>
-                        <p className="text-sm">@alex</p>
-                        <p className="text-sm">4 seconds</p>
-                    </div>
-            
-                    <div className="ml-8 mt-2">This is my tweet!</div>
-            
-                    <div className="flex gap-4 ml-5 my-5 items-center">
-                        <div className="flex gap-1 items-center">
-                            <FaRegCommentDots className="text-2xl font-bold hover:text-blue-500" />
-                            <p className="text-sm">1</p>
-                        </div>
-                        <div className="flex gap-1 items-center">
-                            <FaRegHeart className="text-2xl font-bold hover:text-red-500" />
-                            <p className="text-sm">1</p>
+                {userPosts.map((post) => (
+                    <div key={post.id} className="p-2">
+                        <div className="flex-1 text-left border-b-2 border-black dark:border-white mt-5">
+                            <div className="ml-8 mt-2">{post.caption}</div>
                         </div>
                     </div>
-                </div>
-            
-                {/* Tweet 2 */}
-                <div className="flex-1 text-left border-b-2 border-black dark:border-white mt-5">
-                    <div className="flex gap-2 ml-5 items-center">
-                        <h1 className="text-lg font-bold">Alex</h1>
-                        <p className="text-sm">@alex</p>
-                        <p className="text-sm">4 seconds</p>
-                    </div>
-            
-                    <div className="ml-8 mt-2">This is my tweet!</div>
-            
-                    <div className="flex gap-4 ml-5 my-5 items-center">
-                        <div className="flex gap-1 items-center">
-                            <FaRegCommentDots className="text-2xl font-bold hover:text-blue-500" />
-                            <p className="text-sm">1</p>
-                        </div>
-                        <div className="flex gap-1 items-center">
-                            <FaRegHeart className="text-2xl font-bold hover:text-red-500" />
-                            <p className="text-sm">1</p>
-                        </div>
-                    </div>
-                </div>
-            
-                {/* Tweet 3 */}
-                <div className="flex-1 text-left border-b-2 border-black dark:border-white mt-5">
-                    <div className="flex gap-2 ml-5 items-center">
-                        <h1 className="text-lg font-bold">Alex</h1>
-                        <p className="text-sm">@alex</p>
-                        <p className="text-sm">4 seconds</p>
-                    </div>
-            
-                    <div className="ml-8 mt-2">This is my tweet!</div>
-            
-                    <div className="flex gap-4 ml-5 my-5 items-center">
-                        <div className="flex gap-1 items-center">
-                            <FaRegCommentDots className="text-2xl font-bold hover:text-blue-500" />
-                            <p className="text-sm">1</p>
-                        </div>
-                        <div className="flex gap-1 items-center">
-                            <FaRegHeart className="text-2xl font-bold hover:text-red-500" />
-                            <p className="text-sm">1</p>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
 
             <EditProfile isOpen={openModal} onClose={() => setOpenModal(false)} />
